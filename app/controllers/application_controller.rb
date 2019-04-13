2f52a40624281374fba860a2ec26f6e49d38bf7b
Raghu Betina <raghu@firstdraft.com>
2019-04-13 05:02:53 +0000

    Generate users resource with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :curl_pattern_id, :porosity_id, :density_id, :age_group_id])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:username, :curl_pattern_id, :porosity_id, :density_id, :age_group_id])
+  end
 end
