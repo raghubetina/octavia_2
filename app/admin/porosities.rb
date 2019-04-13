779d1679135b1e493e89ff940afd1c8cfe1ef6de
Raghu Betina <raghu@firstdraft.com>
2019-04-13 05:03:40 +0000

    Whitelist columns in Porosity for ActiveAdmin

--- app/admin/porosities.rb
+++ app/admin/porosities.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Porosity do
+
+ permit_params :level, :description, :position
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
