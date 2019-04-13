c504230dc79a241c604cc8036470a9adc092c73e
Raghu Betina <raghu@firstdraft.com>
2019-04-13 05:03:36 +0000

    Whitelist columns in CurlPattern for ActiveAdmin

--- app/admin/curl_patterns.rb
+++ app/admin/curl_patterns.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register CurlPattern do
+
+ permit_params :number, :letter, :description, :position
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
