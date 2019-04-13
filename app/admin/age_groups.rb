f7b285e7897ddc769068e192cf236703624155ed
Raghu Betina <raghu@firstdraft.com>
2019-04-13 05:03:49 +0000

    Whitelist columns in AgeGroup for ActiveAdmin

--- app/admin/age_groups.rb
+++ app/admin/age_groups.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register AgeGroup do
+
+ permit_params :range, :position
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
