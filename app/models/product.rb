80c71c3e88b8f1962b6cf778ea0097e2220a8476
Raghu Betina <raghu@firstdraft.com>
2019-04-13 05:03:17 +0000

    Direct association between products and ingredients

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   # Direct associations
 
+  has_many   :ingredients,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Product, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:ingredients) }
+
     end
 
     describe "InDirect Associations" do
