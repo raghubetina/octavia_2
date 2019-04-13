ca5072ffcdd016a338776c7f171670c51c46e535
Raghu Betina <raghu@firstdraft.com>
2019-04-13 05:03:52 +0000

    Whitelist columns in Product for ActiveAdmin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Product do
+
+ permit_params :product_category_id, :name, :amazon_asin, :photo
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
